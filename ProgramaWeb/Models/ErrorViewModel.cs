d56776111e3f42e78d100c1eb06ebf6859013e70
Jefferson Lima <chrisjefflima@gmail.com>
2021-02-19 09:53:16 -0300

    inicio

---
+++ ProgramaWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProgramaWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
